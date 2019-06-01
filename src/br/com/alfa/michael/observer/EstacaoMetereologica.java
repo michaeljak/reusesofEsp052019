329daba3765b2b9ac65ac5aa87514f74049fa54f
Aluno <Aluno@172.62.4.107>
2019-06-01 16:59:46 -0300

    Commit aula 01/06 restantes dos exercicios melhorias

--- src/br/com/alfa/michael/observer/EstacaoMetereologica.java
+++ src/br/com/alfa/michael/observer/EstacaoMetereologica.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.alfa.michael.observer;
 
 import java.util.ArrayList;
@@ -10,10 +6,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-/**
- *
- * @author João
- */
+
 public class EstacaoMetereologica {
     private double temperatura;
     private double umidade;
--- src/br/com/alfa/michael/observer/Observer.java
+++ src/br/com/alfa/michael/observer/Observer.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.com.alfa.michael.observer;
 
-/**
- *
- * @author Aluno
- */
 public interface Observer {
     public void atualiza(double temperatura, double pressao, double umidade);
 }
